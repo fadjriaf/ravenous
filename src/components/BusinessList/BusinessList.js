e5759da22f665f524b45a45af2e2b2811bb2e557
Fadjri Alfalah <25312375+fadjriaf@users.noreply.github.com>
2019-12-10 12:44:08 +0700

    ravenous part two
    Passing Information to Components.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,12 +5,11 @@ import './BusinessList.css';
 class BusinessList extends React.Component {
     render() {
         return <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
+            {
+                this.props.businesses.map(business => {
+                    return <Business business={business} />
+                })
+            }
         </div>
     }
 }
