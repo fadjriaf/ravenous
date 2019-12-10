35d6f4f57da62dd4d8e7bf365772a9d8df51eabb
Fadjri Alfalah <25312375+fadjriaf@users.noreply.github.com>
2019-12-10 15:20:54 +0700

    ravenous part four
    Interacting with the Yelp API

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-        term: '',
-        location: '',
-        sortBy: 'best_match'
+      term: '',
+      location: '',
+      sortBy: 'best_match'
     };
 
     this.handleTermChange = this.handleTermChange.bind(this);
@@ -17,64 +17,64 @@ class SearchBar extends React.Component {
     
 
     this.sortByOptions = {
-        'Best Match': 'best_match',
-        'Highest Rated': 'rating',
-        'Most Reviewed': 'review_count'
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
     };
-}
+  }
 
-getSortByClass(sortByOption) {
+  getSortByClass(sortByOption) {
     if (this.state.sortBy === sortByOption) {
-        return 'active';
+      return 'active';
     }
     return '';
-}
+  }
 
-handleSortByChange(sortByOption) {
+  handleSortByChange(sortByOption) {
     this.setState({sortBy: sortByOption});
-}
+  }
 
-handleTermChange(event) {
+  handleTermChange(event) {
     this.setState({term: event.target.value});
-}
+  }
 
-handleLocationChange(event) {
+  handleLocationChange(event) {
     this.setState({location: event.target.value});
-}
+  }
 
-handleSearch(event) {
+  handleSearch(event) {
     this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
 
     event.preventDefault();
-}
+  }
 
-renderSortByOptions() {
+  renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
-        let sortByOptionValue = this.sortByOptions[sortByOption];
-        return (<li className={this.getSortByClass(sortByOptionValue)}
-                    key={sortByOptionValue}
-                    onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
-                    {sortByOption}
-                </li>);
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+                  key={sortByOptionValue}
+                  onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+                {sortByOption}
+             </li>);
     });
-}
+  }
 
-render() {
+  render() {
     return (
-        <div className="SearchBar">
-            <div className="SearchBar-sort-options">
-                <ul>
-                    {this.renderSortByOptions()}
-                </ul>
-            </div>
-            <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-                <input placeholder="Where?" onChange={this.handleLocationChange}/>
-            </div>
-            <div className="SearchBar-submit">
-                <a onClick={this.handleSearch}>Let's Go</a>
-            </div>
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
         </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
+        </div>
+        <div className="SearchBar-submit">
+          <a onClick={this.handleSearch}>Let's Go</a>
+        </div>
+      </div>
     );
   }
 }
